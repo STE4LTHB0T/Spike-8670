91d696b0fdf0f7f0fa8844a75b3846dc7c365b24
XKaliber <srihariguy@gmail.com>
2022-03-08 17:46:12 +0530

    DM Reminders

--- cogs/DM.py
+++ cogs/DM.py
@@ -1,5 +1,6 @@
 import discord, time
 from datetime import datetime
+from dotenv import load_dotenv
 from discord.ext import commands
 
 class DM(commands.Cog):
@@ -15,9 +16,10 @@ class DM(commands.Cog):
   async def beactive(self):
       guild=414057277050585088
       pastDate = 0
+      check_role = get(guild.message.server.roles, name='Shinobi')
       if (datetime.now() - pastDate).days>1:       
           for member in guild.members:
-              if member.discord.Role is None:
+              if check_role not in member.roles:
                   await member.send("Hey! This is Spike from Otaku Nadu OwO. I know this is kinda awkward but man you really should talk there!\nI ain't leaving you alone until you atleast have a role or something.")
           pastDate = datetime.now()
       time.sleep(43200)        
