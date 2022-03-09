51ae5040b226a401576a98cd2c32252100eb41b2
XKaliber <srihariguy@gmail.com>
2022-03-09 15:43:57 +0530

    New Welcome gifs

--- resources/Lists.py
+++ resources/Lists.py
@@ -135,6 +135,35 @@ welcome_reply=['https://media.giphy.com/media/4ilFRqgbzbx4c/giphy.gif',
 
     'https://giffiles.alphacoders.com/102/102768.gif',
 
-    'https://giffiles.alphacoders.com/102/102485.gif'
+    'https://giffiles.alphacoders.com/102/102485.gif',
+    
+    'https://gfycat.com/infantilewarpedarmadillo',
+
+    'https://64.media.tumblr.com/4bd075db973b32623e3ebe0313997dae/817578b793c49935-38/s500x750/6c80b0d1d600d0dbaba87b1704659665f1abdec5.gifv',
+
+    'https://64.media.tumblr.com/bdf8d6ec074d2031785273f3ce16ea6f/d5fc5643569cc175-4c/s500x750/c424cc775f9c2971cd8fce5f1ee98eae8920e085.gifv',
+
+    'https://i.imgur.com/ExgIMjG.gif',
+
+    'https://i.imgur.com/ptCZBkV.gif',
+
+    'https://i.imgur.com/1cleR4E.gif',
+    
+    'https://i.imgur.com/VMGCzFe.gif',
+
+    'https://i.imgur.com/DUutP2l.gif',
+
+    'https://i.imgur.com/t3GVq93.gif',
+
+    'https://64.media.tumblr.com/7d3ebbb1f32cf5296d4555a1fc182ac0/cb4a12f342102665-14/s500x750/060e62916e189bea3c2113d17cea6eabafb8a5e4.gifv'
+
+    'https://c.tenor.com/ZwsoBWFiRVYAAAAC/cowboy-bebop-anime.gif',
+
+    'https://64.media.tumblr.com/fa342ffd75109ffc24ad4fa412bfc508/tumblr_nihmtrslbW1s8diwoo2_500.gifv',
+
+    'https://c.tenor.com/bOmhwto1JpIAAAAd/faye-valentine.gif',
+    'https://64.media.tumblr.com/041e44733f1d9df56eabb51fac5f6845/tumblr_milkvtQZQx1r72ht7o1_500.gifv',
+    'https://i.imgur.com/VBzhxCF.gif'
+
 
     ]
