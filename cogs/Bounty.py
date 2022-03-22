686872031d642ce552d41a44f293663e32dcfb2f
STE4LTHB0T <tansinjansheer@gmail.com>
2022-03-23 00:45:58 +0530

    Exception Fix

--- cogs/Bounty.py
+++ cogs/Bounty.py
@@ -28,38 +28,41 @@ class Bounty(commands.Cog):
 	
 	@commands.Cog.listener()
 	async def on_message(self,message):
-		stats = ranking.find_one({"id":message.author.id, "guild id":message.author.guild.id})
-		if not message.author.bot:
-			if not message.guild:
-				return
-			if message.channel.id in spam_channels:
-				return
-			if "check ratelimit":
-				ratelimit = self.get_ratelimit(message)
-			if ratelimit is None:
-				if stats is None:
-					newuser = {"name":message.author.name,"id":message.author.id, "guild id":message.author.guild.id, "guild name":message.author.guild.name, "xp": 0, "woolongs":0}
-					ranking.insert_one(newuser)
-				else:
-					xp=stats["xp"]+100
-					woolongs=stats["woolongs"]+100	
-					ranking.update_one({"id":message.author.id, "guild id":message.author.guild.id},{"$set":{"xp":xp}})
-					ranking.update_one({"id":message.author.id, "guild id":message.author.guild.id},{"$set":{"woolongs":woolongs}})
-					lvl = 0
-					while True:
-						if xp < ((50*(lvl**2))+(50*lvl)):
-							break
-						lvl +=1
-					xp -= ((50*((lvl-1)**2))+(50*(lvl-1)))
-					if xp == 0:
-						await message.channel.send(f"**Attention! The level of {message.author.mention} has been raised to Level {lvl}!**")
-						if message.guild.id == 414057277050585088:
-							for i in range(len(level)):
-								if lvl == levelnum[i]:
-									await message.author.add_roles(discord.utils.get(message.author.guild.roles, name=level[i]))
-									bounty_level=discord.Embed(title="Announcement!",description=f"Attention! {message.author.mention} is now declared as **{level[i]}**!", color=discord.Color.red())
-									bounty_level.set_thumbnail(url=message.author.avatar_url)
-									await message.channel.send(embed=bounty_level)
+		try:
+			stats = ranking.find_one({"id":message.author.id, "guild id":message.author.guild.id})
+			if not message.author.bot:
+				if not message.guild:
+					return
+				if message.channel.id in spam_channels:
+					return
+				if "check ratelimit":
+					ratelimit = self.get_ratelimit(message)
+				if ratelimit is None:
+					if stats is None:
+						newuser = {"name":message.author.name,"id":message.author.id, "guild id":message.author.guild.id, "guild name":message.author.guild.name, "xp": 0, "woolongs":0}
+						ranking.insert_one(newuser)
+					else:
+						xp=stats["xp"]+100
+						woolongs=stats["woolongs"]+100	
+						ranking.update_one({"id":message.author.id, "guild id":message.author.guild.id},{"$set":{"xp":xp}})
+						ranking.update_one({"id":message.author.id, "guild id":message.author.guild.id},{"$set":{"woolongs":woolongs}})
+						lvl = 0
+						while True:
+							if xp < ((50*(lvl**2))+(50*lvl)):
+								break
+							lvl +=1
+						xp -= ((50*((lvl-1)**2))+(50*(lvl-1)))
+						if xp == 0:
+							await message.channel.send(f"**Attention! The level of {message.author.mention} has been raised to Level {lvl}!**")
+							if message.guild.id == 414057277050585088:
+								for i in range(len(level)):
+									if lvl == levelnum[i]:
+										await message.author.add_roles(discord.utils.get(message.author.guild.roles, name=level[i]))
+										bounty_level=discord.Embed(title="Announcement!",description=f"Attention! {message.author.mention} is now declared as **{level[i]}**!", color=discord.Color.red())
+										bounty_level.set_thumbnail(url=message.author.avatar_url)
+										await message.channel.send(embed=bounty_level)
+		except:
+			pass
 
 	@commands.command()
 	async def bounty(self,ctx,member:discord.Member=None):
