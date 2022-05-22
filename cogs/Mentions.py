4329fddccab4a5f4885c99b27db65bea899907ca
STE4LTHB0T <tansinjansheer@gmail.com>
2022-05-22 23:39:03 +0530

    Delete message

--- cogs/Mentions.py
+++ cogs/Mentions.py
@@ -18,19 +18,19 @@ class Mentions(commands.Cog):
         if message.author == self.client.user:
           return
         if message.author.id == 310366898678136832:
-          await message.channel.send(f"{message.author.mention} Bot Instrumentality Project Banzai🥂")
+          await message.channel.send(f"{message.author.mention} Bot Instrumentality Project Banzai🥂", delete_after=5)
           return
 
         elif message.author.id == 463780399437447200:
-          await message.channel.send(f"Hisashiburi {message.author.mention}!")
-          await message.channel.send("https://media.giphy.com/media/5gK1hvwoutPnG/giphy.gif")
+          await message.channel.send(f"Hisashiburi {message.author.mention}!", delete_after=5)
+          await message.channel.send("https://media.giphy.com/media/5gK1hvwoutPnG/giphy.gif", delete_after=5)
           return
         
         elif message.author.id == message.guild.owner.id:
-          await message.channel.send("big fan, saar<:FeelsHypeMan:477783011887153182>")
+          await message.channel.send("Hello, Chief!<:FeelsFedoraMan:477838638185578496>", delete_after=5)
 
         else:
-          await message.channel.send(random.choice(mentions_reply))
+          await message.channel.send(random.choice(mentions_reply), delete_after=15)
 
 
 def setup(client):
